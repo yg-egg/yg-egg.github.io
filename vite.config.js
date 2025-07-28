2908a8fd91550291c38485caaadba3efe370d42e
Caner Atici <caneratici.cs@gmail.com>
2025-07-28 14:50:40 -0700

    removed redirects, changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // After build, copy _redirects from src to dist
-      plugins: [
-        {
-          name: 'copy-redirects',
-          writeBundle() {
-            const src = path.resolve(__dirname, 'src/_redirects')
-            const dest = path.resolve(__dirname, 'dist/_redirects')
-            fs.copyFileSync(src, dest)
-          }
-        }
-      ]
-    }
+  server: {
+    historyApiFallback: true,
   }
 })
